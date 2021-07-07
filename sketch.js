a64b95e3cc9e85f0cf4b3bb3dd4ce5220cc162b5
UnknownSnake <86217333+Anay-Parikh@users.noreply.github.com>
2021-07-06 20:41:55 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -58,7 +58,7 @@ function keyPressed() {
 }
 
 function applyForceBall() {
-	Matter.Body.applyForce(ball, {x: ball.position.x, y: ball.position.y}, {x: 25, y: -4})
+	Matter.Body.applyForce(ball, {x: ball.position.x, y: ball.position.y}, {x: 80, y: -7})
 }
 
 
